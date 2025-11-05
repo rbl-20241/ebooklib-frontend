be2b8d0ba9a23e50a88d3cf5cc2122f8345bcbf7
rene <blom1965@gmail.com>
2025-11-05 15:31:36 +0100

    panel om filetree gezet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
     providePrimeNG({
       theme: {
         preset: Aura
--- src/app/filetree/filetree.component.ts
+++ src/app/filetree/filetree.component.ts
@@ -2,12 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TreeNode } from 'primeng/api';
 import { FiletreeService } from './filetree.service';
 import { Tree } from 'primeng/tree';
+import { Panel } from 'primeng/panel';
 
 @Component({
   selector: 'app-filetree',
   templateUrl: './filetree.component.html',
   styleUrls: ['./filetree.component.css'],
-  imports: [ Tree ],
+  imports: [ Tree, Panel ],
   providers: [ FiletreeComponent ]
 })
 export class FiletreeComponent implements OnInit {
