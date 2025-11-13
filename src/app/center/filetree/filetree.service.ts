b96c8dffc0432db71690a2bf7f35be480748a214
rene <blom1965@gmail.com>
2025-11-13 15:20:10 +0100

    Deprecate toPromise vervangen

--- src/app/center/filetree/filetree.service.ts
+++ src/app/center/filetree/filetree.service.ts
@@ -2,15 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { TreeNode } from 'primeng/api';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class FiletreeService {
 
   constructor(private http: HttpClient) { }
 
-  getBooks() {
-    return this.http.get<any>('http://localhost:8080/getbooks')
-      .toPromise()
-      .then(res => <TreeNode[]>res.data);
+  async getBooks(): Promise<TreeNode[]> {
+    const res = await firstValueFrom(
+      this.http.get<any>('http://localhost:8080/getbooks')
+    );
+    return res.data as TreeNode[];
   }
 }
